43071c720643b2df32061404b1beef473a4c0c29
xiao <1066245153@qq.com>
2020-06-09 20:33:05 +0800

    好友类

--- app/src/main/java/cn/edu/scujcc/duoduo/FriendActivity.java
+++ app/src/main/java/cn/edu/scujcc/duoduo/FriendActivity.java
@@ -2,13 +2,29 @@ package cn.edu.scujcc.duoduo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FriendActivity extends AppCompatActivity {
+    private TextView message;
+    private List list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
+
+        this.message= findViewById(R.id.message);
+        list = new List(FriendActivity.this, p -> {
+            //跳转到新界面，使用意图Intent
+            Intent intent = new Intent(FriendActivity.this, MainActivity.class);
+            List c = lab.getChannel(p);
+            intent.putExtra("List",c);
+            startActivity(intent);
+        });
+
+
     }
-}
+    }
+
